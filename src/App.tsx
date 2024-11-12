def7083272fefe6df4560440ebb4909552f8b5e7
Emmanuel Guim III <emmanuelguim.third@gmail.com>
2024-11-12 14:02:03 +0800

    Installed and setup redux 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './hooks/useRedux';
+import { RootState } from './store';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useAppSelector((state: RootState) => state.counter.value);
+  const dispatch = useAppDispatch();
 
   const buttonClass =
     'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded min-w-14';
@@ -11,14 +13,14 @@ function App() {
     <div className="flex items-center justify-center min-h-screen gap-4">
       <button
         className={buttonClass}
-        onClick={() => setCount((count) => count - 1)}
+        onClick={() => dispatch({ type: 'counter/increment' })}
       >
         -
       </button>
       <h1 className="font-bold text-2xl">count is {count}</h1>
       <button
         className={buttonClass}
-        onClick={() => setCount((count) => count + 1)}
+        onClick={() => dispatch({ type: 'counter/decrement' })}
       >
         +
       </button>
