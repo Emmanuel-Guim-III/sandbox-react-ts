faf845323f9cf8bd4a10db6a0b61157da7b3dbf6
Emmanuel Guim III <emmanuelguim.third@gmail.com>
2024-11-12 11:53:21 +0800

    Simple increment and decrement buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
 import { useState } from 'react';
 import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const buttonClass =
+    'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded min-w-14';
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bg-purple-300">Vite + React + Tailwind</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex items-center justify-center min-h-screen gap-4">
+      <button
+        className={buttonClass}
+        onClick={() => setCount((count) => count - 1)}
+      >
+        -
+      </button>
+      <h1 className="font-bold text-2xl">count is {count}</h1>
+      <button
+        className={buttonClass}
+        onClick={() => setCount((count) => count + 1)}
+      >
+        +
+      </button>
+    </div>
   );
 }
 
